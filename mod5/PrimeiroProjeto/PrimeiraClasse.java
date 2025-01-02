5477486e931c61012f8de6f6a042547101e179a3
Dowglas <bittencourtdouglas41@gmail.com>
2025-01-01 22:44:30 -0300

    upload dos arquivos da tarefa do modulo 5

---
+++ mod5/PrimeiroProjeto/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package mod5.PrimeiroProjeto;
+
+public class PrimeiraClasse{
+    public static void main(String[] args){
+        String myName = args[0];
+
+        System.out.println("Olá, " + myName + " Tudo bem com voce?");
+    }
+}
